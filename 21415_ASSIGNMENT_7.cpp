a9b1e24f6c8f2a43fd1bb9922317fd07ec2f4908
Vedant Kokane (PICT) duplicate <120434854+Vedant-kokane-PICT@users.noreply.github.com>
2022-12-13 08:06:42 +0530

    Update 21415_ASSIGNMENT_7.cpp
    Write C++ program for storing binary number using doubly
    linked lists. Write functions- a) To compute 1„s and 2„s complement b)
    Add two binary numbers.

--- 21415_ASSIGNMENT_7.cpp
+++ 21415_ASSIGNMENT_7.cpp
@@ -1,3 +1,7 @@
+Write C++ program for storing binary number using doubly
+linked lists. Write functions- a) To compute 1„s and 2„s complement b)
+Add two binary numbers
+
 //============================================================================
 // Name        : DSL_ASSIGNMENT_7.cpp
 // Author      : VEDANT KOKANE
