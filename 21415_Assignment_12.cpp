29fc4cbb395d39ad334a3f13bbbdeef6c10c8073
Vedant Kokane (PICT) duplicate <120434854+Vedant-kokane-PICT@users.noreply.github.com>
2022-12-13 08:12:58 +0530

    Update 21415_Assignment_12.cpp
    Write program to implement a priority queue in C++ using
    an order list/array to store the items in the queue. Create a
    class that includes the data items (which should be template)
    and the priority (which should be int). The order list/array
    should contain these objects, with operator <= overloaded so
    that the items with highest priority appear at the beginning of
    the list/array (which will make it relatively easy to retrieve the
    highest item.)

--- 21415_Assignment_12.cpp
+++ 21415_Assignment_12.cpp
@@ -1,3 +1,13 @@
+Write program to implement a priority queue in C++ using
+an order list/array to store the items in the queue. Create a
+class that includes the data items (which should be template)
+and the priority (which should be int). The order list/array
+should contain these objects, with operator <= overloaded so
+that the items with highest priority appear at the beginning of
+the list/array (which will make it relatively easy to retrieve the
+highest item.)
+
+
 //============================================================================
 // Name        : 21415_Assignment_12.cpp
 // Author      : Vedant Kokane
