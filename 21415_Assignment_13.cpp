0202a3a3e08b50dc7b57bbbf120e8213816884c9
Vedant Kokane (PICT) duplicate <120434854+Vedant-kokane-PICT@users.noreply.github.com>
2022-12-13 08:14:23 +0530

    Update 21415_Assignment_13.cpp

--- 21415_Assignment_13.cpp
+++ 21415_Assignment_13.cpp
@@ -1,3 +1,8 @@
+A double-ended queue (deque) is a linear list in which additions and deletions may be
+made at either end. Obtain a data representation mapping a deque into a one dimensional array. 
+Write C++ program to simulate deque with functions to add and delete elements from either end of the deque.
+
+
 //============================================================================
 // Name        : 21415_Assignment_13.cpp
 // Author      : Vedant Kokane
