de3a9ea4a3e51f3201f53a8471db4c76ef851ec5
Vedant Kokane (PICT) duplicate <120434854+Vedant-kokane-PICT@users.noreply.github.com>
2022-12-13 08:09:17 +0530

    Update 21415_Assignment_10.cpp
    Implement C++ program for expression conversion as infix to
    postfix and its evaluation using stack based on given
    conditions: 1. Operands and operator, both must be single
    character. 2. Input Postfix expression must be in a desired
    format. 3. Only '+', '-', '*' and '/ ' operators are expected.

--- 21415_Assignment_10.cpp
+++ 21415_Assignment_10.cpp
@@ -1,3 +1,9 @@
+Implement C++ program for expression conversion as infix to
+postfix and its evaluation using stack based on given
+conditions: 1. Operands and operator, both must be single
+character. 2. Input Postfix expression must be in a desired
+format. 3. Only '+', '-', '*' and '/ ' operators are expected.
+
 //============================================================================
 // Name        : 21415_Assignment_10.cpp
 // Author      : Vedant Kokane
