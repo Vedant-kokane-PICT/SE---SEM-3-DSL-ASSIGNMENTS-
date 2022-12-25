d49106fc866ab19c1c2998e78e2f85e5844f60d0
Vedant Kokane (PICT) duplicate <120434854+Vedant-kokane-PICT@users.noreply.github.com>
2022-12-25 19:07:09 +0530

    Update 21415_Assignment_9.cpp

--- 21415_Assignment_9.cpp
+++ 21415_Assignment_9.cpp
@@ -74,15 +74,15 @@ int main(){
     cout<<"Enter a expression"<<endl;
 	getline(cin,s);
     for(int i=0;i<s.length();i++){
-    if(s1.empty()){
-        s1.push(s[i]);
-    }
-    else if((s1.top1() == '(' && s[i] == ')') || (s1.top1() == '{' && s[i] == '}') ||(s1.top1() == '[' && s[i] == ']')){
-        s1.pop();
-    }
-    else if(s[i] == '(' || s[i] == '{' || s[i] == '['){
-        s1.push(s[i]);
-    }
+	    if(s1.empty()){
+		s1.push(s[i]);
+	    }
+	    else if((s1.top1() == '(' && s[i] == ')') || (s1.top1() == '{' && s[i] == '}') ||(s1.top1() == '[' && s[i] == ']')){
+		s1.pop();
+	    }
+	    else if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+		s1.push(s[i]);
+	    }
     }
     
     if(s1.empty()){
